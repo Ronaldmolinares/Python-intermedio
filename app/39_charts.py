3aff2c64970e1a7da792f03f4b851926a5e07d05
Ronaldmolinares <108416072+Ronaldmolinares@users.noreply.github.com>
2026-01-04 18:40:14 -0500

    grafica de barras y pastel

---
+++ app/39_charts.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+
+def generate_bar_chart(labels, values):
+    fig, ax = plt.subplots()
+    ax.bar(labels, values)
+    plt.show()
+
+
+def generate_pie_chart(labels, values):
+    fig, ax = plt.subplots()
+    ax.pie(values, labels=labels, autopct="%1.1f%%")
+    plt.show()
+
+
+if __name__ == "__main__":
+    labels = ["a", "b", "c"]
+    values = [100, 200, 300]
+    # generate_bar_chart(labels, values)
+    generate_pie_chart(labels, values)
